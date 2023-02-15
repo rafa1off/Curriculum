49c6b3752a20dc400848b0882855eb11c21abade
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-02-15 08:54:31 -0300

    Adicionando swiper

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const conteudos = document.querySelectorAll('.conteudos');
 
 conteudos.onclick = "";
+
+const swiper = new Swiper('.conteudo', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
