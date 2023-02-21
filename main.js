dc1e8c31c806f1ad036d866acbfd8da6c79927de
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-02-21 19:46:45 -0300

    Touch

--- main.js
+++ main.js
@@ -14,6 +14,20 @@ for (i = 0; i < conteudos.length; i++) {
     }
 }
 
+for (i = 0; i < conteudos.length; i++) {
+    const cont = conteudos[i];
+    const cont1 = cont.classList[1];
+    const cont2 = `.${cont1}`;
+
+    cont.ontouchstart = ()=> {
+        expandBox(cont2);
+        
+        setTimeout(() => {
+            retornaBox(cont2)
+        }, 600)
+    }
+}
+
 function expandBox(divConteudo) {
     const container = document.querySelector(divConteudo)
     container.classList.add('conteudos-hover')    
